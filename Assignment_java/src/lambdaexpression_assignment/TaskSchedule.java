dbf72da467d46fc2f1a182528de3b46d9bb5de44
santh <santh@Santhosh>
2025-08-29 09:09:40 +0530

    Assignment on lambda

---
+++ Assignment_java/src/lambdaexpression_assignment/TaskSchedule.java
@@ -0,0 +1,22 @@
+package lambdaexpression_assignment;
+
+public class TaskSchedule {
+
+class TaskScheduler {
+    public void scheduleTask(Task task, int intervalInSeconds) {
+        Thread thread = new Thread(() -> {
+            try {
+                while (true) {
+                    task.execute();   // Run the task
+                    Thread.sleep(intervalInSeconds * 1000); // Wait for interval
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Task interrupted: " + e.getMessage());
+            }
+        });
+        thread.start();
+    }
+}
+
+
+}
